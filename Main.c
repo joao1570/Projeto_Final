646e1cb5f5d1f30c8c70cf59a57f32f871b26d65
Joao Sergio <joao.f.sergio@gmail.com>
2026-08-19 13:53:10 +0100

    Adicionado menu inicial do jogo

--- Main.c
+++ Main.c
@@ -2,7 +2,33 @@
 
 int main() {
 
-    printf("Projeto Final\n");
+    char nome[50];
+    int opcao;
+
+    printf("=================================\n");
+    printf("        BATALHA NAVAL\n");
+    printf("=================================\n\n");
+
+    printf("Introduz o teu nome: ");
+    scanf("%49s", nome);
+
+    printf("\nBem-vindo, %s!\n", nome);
+
+    printf("\n1 - Jogar\n");
+    printf("2 - Sair\n");
+
+    printf("\nEscolhe uma opcao: ");
+    scanf("%d", &opcao);
+
+    if (opcao == 1) {
+        printf("\nO jogo vai comecar!\n");
+    }
+    else if (opcao == 2) {
+        printf("\nA sair do jogo...\n");
+    }
+    else {
+        printf("\nOpcao invalida.\n");
+    }
 
     return 0;
 }
--- log.md
+++ log.md
@@ -5,3 +5,9 @@
 - Configuração do GitHub.
 - Criação do ficheiro Main.c.
 - Criação do ficheiro .gitignore.
+
+## Sessão 2
+- Escolha do jogo Batalha Naval.
+- Criação do menu inicial.
+- Adicionado nome do jogador.
+- Adicionadas opções para jogar ou sair.
